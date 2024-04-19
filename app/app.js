947cd8e83c807b7c91d9410dcd6be883cc473054
Yuri Bychkov <yuribychkov15@gmail.com>
2024-04-19 19:16:56 -0400

    Merged successfully

--- app/app.js
+++ app/app.js
@@ -1,16 +1,27 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 9000;
+
+app.set('views', './app/views');
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.render('index.ejs', { username: 'Guest' });
+    res.render('index', { username: 'Guest' });
 });
-app.post('/greet', (req, res) => {
+
+app.get('/about', (req, res) => {
     const { username } = req.body;
-    res.render('index.ejs', { username }); 
+    res.render('about', { username }); 
 });
+
+app.get('/calculator', (req, res) => {
+    const { username } = req.body;
+    res.render('calculator', { username }); 
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
